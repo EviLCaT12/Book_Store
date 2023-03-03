7dafbfd7fe6d7f57a550e4ba39bf63b374568324
EviLCaT12 <kuznetsov.timofey.2003@mail.ru>
2023-03-03 18:02:00 +1000

    add sln, describe Book

---
+++ domain/Store/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Store;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+
+    public Book(int id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
+}
