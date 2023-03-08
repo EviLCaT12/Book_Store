b7a105f42d955572c60dc8ce655b389c23241097
Corey1337 <kolikendr@gmail.com>
2023-03-09 00:29:08 +1000

    base classes

--- domain/Store/Comment.cs
+++ domain/Store/Comment.cs
@@ -2,5 +2,22 @@
 
 public class Comment
 {
-    
+    private int Id { get; }
+    public User Author { get; set; }
+    public DateOnly Date { get; set; }
+    public Book Book { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Rating { get; set; }
+
+    public Comment(int id, User author, DateOnly date, Book book, string title, string description, int rating)
+    {
+        Id = id;
+        Author = author;
+        Date = date;
+        Book = book;
+        Title = title;
+        Description = description;
+        Rating = rating;
+    }
 }
--- domain/Store/Order.cs
+++ domain/Store/Order.cs
@@ -2,5 +2,18 @@
 
 public class Order
 {
-    
+    private int Id { get; }
+    public User User { get; set; }
+    public DateOnly Date { get; set; }
+    public List<Book> ListOfBook { get; set; }
+    public string Status { get; set; }
+
+    public Order(int id, User user, DateOnly date, List<Book> listOfBook, string status)
+    {
+        Id = id;
+        User = user;
+        Date = date;
+        ListOfBook = listOfBook;
+        Status = status;
+    }
 }
