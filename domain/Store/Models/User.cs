db44dc0e93419a5339ff7cf82e6a49d886c96f5f
Corey1337 <kolikendr@gmail.com>
2023-03-10 01:32:31 +1000

    User validation

--- domain/Store/Models/User.cs
+++ domain/Store/Models/User.cs
@@ -5,12 +5,28 @@ namespace Store;
 public class User
 {
     private int Id { get; }
+
+    [Required(ErrorMessage = "Full Name is required")]
     public string FullName { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
     public string Password { get; set; }
+
+    [Required(ErrorMessage = "Birth Day is required")]
+    [DataType(DataType.Date, ErrorMessage = "Incorrect date")]
     public DateOnly BirthDay { get; set; }
+
+    [Required(ErrorMessage = "Phone is required")]
+    [RegularExpression(@"^(\+7|7|8){1}\ ?\(?[0-9]{3}\)?\ ?[0-9]{3}\ ?\-?\ ?[0-9]{2}\ ?\-?\ ?[0-9]{2}$", ErrorMessage = "Incorrect phone number")]
     public string Phone { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Incorrect email address")]
     public string Email { get; set; }
-    public List<Book> ListFavoriteBook { get; set; }
+
+
+    public List<Book> ?ListFavoriteBook { get; set; }
 
     public User(int id, string fullName, string password, DateOnly birthday, string phone, string email)
     {
@@ -20,5 +36,6 @@ public class User
         BirthDay = birthday;
         Phone = phone;
         Email = email;
+        
     }
 }
