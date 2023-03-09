47c417d316f42d1b3f0eb185f47b76939825ae57
Corey1337 <kolikendr@gmail.com>
2023-03-10 01:34:43 +1000

    test branch

---
+++ Test/Test.cs
@@ -0,0 +1,38 @@
+﻿using Store;
+using System.ComponentModel.DataAnnotations;
+
+namespace Store;
+class Test
+{
+    static void Main(string[] args)
+    {
+        CreateUser(123, "Pash", "asdfghhj", DateOnly.Parse("12.12.1212"), "791467232-27", "123@mail.com");
+    }
+
+    static bool CreateUser(int id, string fullName, string password, DateOnly birthday, string phone, string email)
+    {
+        User user = new User(id, fullName, password, birthday, phone, email);
+
+        var context = new ValidationContext(user);
+        var result = new List<ValidationResult>();
+
+        if (!Validator.TryValidateObject(user, context, result, true))
+        {
+            Console.WriteLine("Failed to create object User");
+            foreach (var error in result)
+            {
+                Console.WriteLine(error);
+            }
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Object User created success");
+
+            //Add user to db
+
+            return true;
+        }
+    }
+}
+
