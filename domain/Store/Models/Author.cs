97ec861e39afaa3c22546d6228625e48bbbf294e
EviLCaT12 <kuznetsov.timofey.2003@mail.ru>
2023-03-13 02:00:39 +1000

    add validationf for book, author,comment

--- domain/Store/Models/Author.cs
+++ domain/Store/Models/Author.cs
@@ -1,16 +1,21 @@
-﻿using System;
-namespace Store;
+﻿using System.ComponentModel.DataAnnotations;
+namespace Store.Models;
 
 public class Author
 {
+    [Key]
     private int Id { get; }
+    [Required(ErrorMessage = "Full name is required")]
     public string Fullname { get; set; }
-    public string YearsOfLife { get; set;}
+    [DataType(DataType.Date, ErrorMessage = "Incorrect date")]
+    [Required(ErrorMessage = "Years of life is required")]
+    public DateOnly YearsOfLife { get; set;}
     public List<Genre> ListOfGenre { get; set; }
+    [Required(ErrorMessage = "Description is required")]
     public string Description { get; set; }
     public List<Book> ListOfBook { get; set; }
 
-    public Author(int id, string fullname, string yearsOfLife, List<Genre> listOfGenre, string description,
+    public Author(int id, string fullname, DateOnly yearsOfLife, List<Genre> listOfGenre, string description,
         List<Book> listOfBook)
     {
         Id = id;
